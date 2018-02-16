ad92afd507a432d315de1f4abd5f91fc071ee4dc
Jennifer Stoos <jstoos@bitlaw.com>
2018-02-16 18:47:36 +0000

    Stubbing out CLI interface with user and Scraper class

--- patents/lib/patents/cli.rb
+++ patents/lib/patents/cli.rb
@@ -1,13 +1,51 @@
 #The Controller
 
 class Patents::CLI
-  def call
-    puts "This is a patent"
-  end
 
+  #Ask the user for a patent number,
+  input = ""
+  number = ""
 
+  until input == "exit" do
 
+    puts "Please enter a patent number: (to exit enter 'exit')"
+    #Get the user input of a patent number and strip it
+    input = gets.strip
 
+    #Check it is a valid number
+    if input.to_i.between?(100000, 99999999)
 
+    #Take that number, create a patent instantiation and return a patent title
+     puts "Here's that patent!"
+     #"Patent number #{input} is entitled: " Scraper.new(number).title
 
-end
+     puts "What would you like to see? (Please enter a number)"
+      "1. Inventor(s) , 2. Application number, 3. Publication Date, 4. Filing date, 5. Assignee, 6. Primary Class, 7. Exit, 8. Enter a New Patent number"
+
+      number = gets.strip
+        while !number.to_i.between?(1, 8)
+          puts "Please enter a number between 1 and 9:"
+          number = gets.strip
+        end
+      if number.to_i.between?(1, 6)
+        #Find the thing they want using Scraper class
+        if number == "1"
+          puts "Inventors: "
+        elsif number == "2"
+          puts "Application number: "
+        elsif number == "3"
+          puts "Publication Date: "
+        elsif number== "4"
+          puts "Filing date: "
+        elsif number == "5"
+          puts "Assignee: "
+        elsif number == "6"
+          puts "Primary Class: "
+        elsif number == 7
+        input = "exit"
+        end
+      end #end of if
+    end #End of if input statement
+  end #End of until loop
+
+end #End of class
